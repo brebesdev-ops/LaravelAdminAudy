57e8fab7e7050a657f9d284fe64875285955ecf5
brebesdev-ops <brebesdev@gmail.com>
2020-03-20 09:33:20 +0700

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Hello</p>
+
+    <div class="content">
+    	<table class="table table-bordered">
+    		<thead>
+    			<tr>
+    				<th>No</th>
+    			</tr>
+    		</thead>
+    		<tbody>
+    			<tr>
+    				<td>1</td>
+    			</tr>
+    		</tbody>
+    	</table>
+    </div>
+@stop
